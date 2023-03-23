1f1a1dcebccc126647fcdd4a60155deba40ac5cb
Ian Eldred Pudney <ipudney@umich.edu>
2023-03-23 15:13:08 -0700

    Update brew package to v0.0.30 (phase 1)

--- Casks/teclada.rb
+++ Casks/teclada.rb
@@ -1,12 +1,12 @@
 cask "teclada" do
-    version "0.0.29"
+    version "0.0.30"
 
     if Hardware::CPU.arm?
-        sha256 "9b067794162b659e4504137c3ea4ec8cbaca46e24d6bc00349c82d9fec33c0b8"
-        url "https://github.com/Teclada/teclada/releases/download/0.0.29/teclada_mac_arm64.tar.gz"
+        #sha256 "9b067794162b659e4504137c3ea4ec8cbaca46e24d6bc00349c82d9fec33c0b8"
+        url "https://github.com/Teclada/teclada/releases/download/#{version}/teclada_mac_arm64.tar.gz"
     elsif Hardware::CPU.intel?
-        url "https://github.com/Teclada/teclada/releases/download/0.0.29/teclada_mac_x86_64.tar.gz"
-        sha256 "654bf5a3648c733d721af40cb6975e62fcefb37c5f96efed77c96ed52ee5872c"
+        url "https://github.com/Teclada/teclada/releases/download/#{version}/teclada_mac_x86_64.tar.gz"
+        #sha256 "654bf5a3648c733d721af40cb6975e62fcefb37c5f96efed77c96ed52ee5872c"
     end
         
     name "Teclada"
