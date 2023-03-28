c635fed8298154b98153dc93ca43776ddc491310
Ian Eldred Pudney <ipudney@umich.edu>
2023-03-28 15:09:28 -0700

    Add sha256 to teclada cask

--- Casks/teclada.rb
+++ Casks/teclada.rb
@@ -2,11 +2,11 @@ cask "teclada" do
     version "0.0.30"
 
     if Hardware::CPU.arm?
-        #sha256 "9b067794162b659e4504137c3ea4ec8cbaca46e24d6bc00349c82d9fec33c0b8"
+        sha256 "ccd1325b145732ab73d00d11329631d74bd3b16aeb0ffdffbf15af318e033337"        
         url "https://github.com/Teclada/teclada/releases/download/#{version}/teclada_mac_arm64.tar.gz"
     elsif Hardware::CPU.intel?
         url "https://github.com/Teclada/teclada/releases/download/#{version}/teclada_mac_x86_64.tar.gz"
-        #sha256 "654bf5a3648c733d721af40cb6975e62fcefb37c5f96efed77c96ed52ee5872c"
+        sha256 "5b96cce4cc6e8c2795f333990fcba8f13de94db3ae8068550adf462999787075"
     end
         
     name "Teclada"
