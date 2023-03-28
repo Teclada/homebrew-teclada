f00e46ad73d21f9cda4b27d4c03efa826ed06906
Ian Eldred Pudney <ipudney@umich.edu>
2023-03-28 15:12:40 -0700

    Try incrementing formula version along with Cask version

--- Formula/teclada_base_formula.rb
+++ Formula/teclada_base_formula.rb
@@ -2,7 +2,7 @@ class TecladaBaseFormula < Formula
   desc "An empty formula."
   homepage "https://teclada.com/"
   url "https://github.com/Teclada/homebrew-teclada/archive/26513edf8dfc32e6541e2f74680b3e9fffdfc735.zip"
-  version "1.0.0"
+  version "1.0.31"
   sha256 "9464078c6cfc9379430c81e5185922f64350f47f7771204702d7016dfcc4467e"  
   license ""
 
