1a65875751dc93524b3b65f78c97f019a20df547
runner <runner@Mac-1683595176709.local>
2023-05-09 01:41:50 +0000

    Action - release action_0.0.42_1683596510

--- Casks/teclada.rb
+++ Casks/teclada.rb
@@ -1,12 +1,12 @@
 cask "teclada" do
-    version "0.0.41"
+    version "0.0.42"
 
     if Hardware::CPU.arm?  
         url "https://github.com/Teclada/teclada/releases/download/#{version}/teclada_mac_arm64.tar.gz"
-        sha256 "7d04151235fb24d3d741e45ff22bbcabac12843479f5a25f4e61134d54accb70"
+        sha256 "96d858d9d20e082fbd7ae947c68526f635f830152fc3d30f39e542f603401196"
     elsif Hardware::CPU.intel?
         url "https://github.com/Teclada/teclada/releases/download/#{version}/teclada_mac_x86_64.tar.gz"
-        sha256 "eb6efc615861ba2e62429cf99788b91f3a72be48de5b0d98223a8b95994e771d"
+        sha256 "f4320bed97b5bbb7a093b49d761e3e56e8e9389bd10c6ba4523620c8c5cd8339"
     end
         
     name "Teclada"
