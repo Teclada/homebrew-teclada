732ddbcd5a1de3108ed6f668e93692c36545e650
Ian Eldred Pudney <ipudney@umich.edu>
2023-06-06 16:14:53 -0400

    version 0.0.53

--- Casks/teclada.rb
+++ Casks/teclada.rb
@@ -1,14 +1,14 @@
 cask "teclada" do
-    version "0.0.44"
+    version "0.0.53"
 
-    if Hardware::CPU.arm?  
+    if Hardware::CPU.arm?
         url "https://github.com/Teclada/teclada/releases/download/#{version}/teclada_mac_arm64.tar.gz"
-        sha256 "c2c7f9447af3fb6100f38b2d074c81ba97cbd87f70ade0e55a0d13a97e30d6af"
+        sha256 "76a9efe922800211babbac2003e2d45245e7059850b5430333cc606e52fc3d4e"
     elsif Hardware::CPU.intel?
         url "https://github.com/Teclada/teclada/releases/download/#{version}/teclada_mac_x86_64.tar.gz"
-        sha256 "4b1112b37e66b7a5e24619e849376bad283067298c8fa1daa885db731521bc1e"
+        sha256 "2b32973bdaa157e1ee9cae821a0be57e73ee535b0d04775f902e9d29c7a10d31"
     end
-        
+
     name "Teclada"
     desc "The modern terminal for the modern developer."
     homepage "https://www.teclada.com/"
@@ -40,4 +40,3 @@ cask "teclada" do
     }
 
   end
-  
